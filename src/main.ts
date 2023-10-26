aac3f7727fc14adce6ac213d90d2c0a7a6f1ef6c
Kewen Silva <kewensilva58@gmail.com>
2023-10-26 16:55:17 -0300

    inserindo as rotas do site sem login

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import router from './routes/index.js'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/views/SobreoNippec.vue
@@ -0,0 +1,1 @@
+<template><div>SobreoNippec</div></template>
