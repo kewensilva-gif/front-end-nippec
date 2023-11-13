45c66705cea61e16ba98ea67e21ce8e6c0f4bc1c
samuelAnjos <samuelanjos@academico.ufs.br>
2023-11-13 13:38:22 -0300

    resposivida aplicada

--- src/views/pesquisa/IniciacaoCientifica.vue
+++ src/views/pesquisa/IniciacaoCientifica.vue
@@ -1,3 +1,34 @@
+
 <template>
-    <div>IniciacaoCientifica</div>
+    <div class="m-28 ty:m-16 bg-secondary-color font-heading text-sm sm:text-base md:text-xl lg:text-2xl">
+        <h2 class="mb-10 text-primary-color text-left leading-8">Iniciação Científica e Tecnológica no Campus de Itabaiana</h2>
+
+    <div class="m-12 bg-secondary-color font-heading">
+        <h2 class="mb-4 text-primary-color">Iniciação Científica e Tecnológica no Campus de Itabaiana</h2>
+
+        <p class="mb-4 text-justify">No âmbito dos cursos de graduação, as atividades de pesquisa são incentivadas através dos programas de iniciação científica (PIBIC) e tecnológica (PIBITI). Esses programas têm como principal objetivo mostrar ao aluno o método científico através de projetos de pesquisa com duração de um ano. O acadêmico é estimulado a ingressar na área da pesquisa científica e tecnológica, desenvolvendo atividades orientadas dentro dos projetos e linhas de pesquisa existentes no Campus e na universidade. Para ingressar nos programas de iniciação científica ou iniciação tecnológica, o acadêmico deve ficar atento aos editais publicados pela Universidade e nos projetos individuais de cada departamento. O estudante deve entrar em contato com os docentes orientadores os quais são responsáveis por enviar projetos de pesquisa para concorrer a bolsas na universidade e agências de fomento. Como a inscrição no programa é feita pelo professor orientador, o estudante deve estabelecer contato com os professores que atuam nas áreas de seu interesse para informar-se sobre bolsas ou sobre sua participação voluntária. É através do professor, que participa de um projeto de pesquisa, de inovação e/ou de desenvolvimento tecnológico e que corresponde aos pré-requisitos estabelecidos em edital, que o estudante pode candidatar-se a uma bolsa de iniciação científica ou tecnológica ou como voluntário. Em todas as modalidades de bolsas é necessário que o estudante cumpra uma carga horária de 20 horas semanais e não possua vínculo empregatício.</p>
+
+        <h3 class="mb-4 text-primary-color">PIBIC/CNPq</h3>
+        <p class="mb-4 text-justify">Programa de Bolsas do Conselho Nacional de Desenvolvimento Científico e Tecnológico (CNPq) do qual a UCS é instituição integrante. Através da bolsa PIBIC/CNPq, o estudante de graduação ingressa como bolsista em um dos projetos de pesquisa da Instituição. A abertura do edital para essa modalidade de bolsa acontece no mês de maio, com vigência de agosto a julho de cada ano. O valor atual da bolsa é de R$ 400,00 mensais. A avaliação e seleção é realizada por comitê externo do qual fazem parte pesquisadores do CNPq. Há também possibilidade de participação no programa como voluntário.</p>
+
+        <h3 class="mb-4 text-primary-color">PIBIC/COPES</h3>
+        <p class="mb-4 text-justify">Programa Institucional de Bolsas de Iniciação Científica da própria universidade cujos recursos financeiros são oriundos do Programa Nacional de Assistência Estudantil – PNAES. Para concorrer a essa bolsa, além de procurar o orientador, os alunos devem comprovar a situação de vulnerabilidade O valor atual da bolsa é de R$ 400,00 mensais. A bolsa também tem vigência de 12 meses. As inscrições para esta modalidade de bolsa são realizadas mediante publicação de edital anual. Há também possibilidade de participação no programa como voluntário.</p>
+
+        <h3 class="mb-4 text-primary-color">PIBITI/CNPq</h3>
+        <p class="mb-4 text-justify">Programa Institucional de Bolsas de Iniciação em Desenvolvimento Tecnológico e Inovação do CNPq visa estimular os jovens do ensino superior nas atividades, metodologias, conhecimentos e práticas próprias ao desenvolvimento tecnológico e processos de inovação. A seleção é feita mediante processo seletivo anual. A bolsa tem vigência por um período de 12 meses e atualmente tem valor mensal de R$ 400,00. Há também possibilidade de participação no programa como voluntário.</p>
+
+        <h3 class="mb-4 text-primary-color">PBIC/FAPITEC</h3>
+        <p class="mb-4 text-justify">Programa institucional de bolsas de iniciação científica (PBIC) da Fundação de Apoio a Pesquisa e à Inovação Tecnológica do Estado de Sergipe (FAPITEC/SE) no qual o estudante de graduação pode ingressar como bolsista em um dos projetos de pesquisa de um pesquisador do campus. O pesquisador é o responsável por enviar o projeto para concorrer no edital dessa agência de fomento. O edital é anual e a bolsa tem duração de 12 meses. O valor atual da bolsa é de R$ 400,00 mensais.</p>
+
+        <h3 class="mb-4 text-primary-color">PIBITI/FAPITEC</h3>
+        <p class="mb-4 text-justify">Programa institucional de bolsas de iniciação em desenvolvimento tecnológico e inovação (PIBITI) da Fundação de Apoio a Pesquisa e à Inovação Tecnológica do Estado de Sergipe (FAPITEC/SE) que visa estimular os jovens do ensino superior nas atividades, metodologias, conhecimentos e práticas próprias ao desenvolvimento tecnológico e processos de inovação. A seleção é feita mediante processo seletivo anual. A bolsa tem vigência por um período de 12 meses e atualmente tem valor mensal de R$ 400,00.</p>
+
+        <h3 class="mb-4 text-primary-color leading-8">Projetos de Pesquisa Individuais</h3>
+        <p class="mb-4 text-justify">O estudante de graduação também pode fazer iniciação científica através da participação em projetos de pesquisa individuais aprovados nos departamentos. Nesse caso, a participação é totalmente voluntária sem remuneração.</p>
+    </div>
+
+        <h3 class="mb-4 text-primary-color">Projetos de Pesquisa Individuais</h3>
+        <p class="mb-4 text-justify">O estudante de graduação também pode fazer iniciação científica através da participação em projetos de pesquisa individuais aprovados nos departamentos. Nesse caso, a participação é totalmente voluntária sem remuneração.</p>
+    </div>
 </template>
+
